5d0538b213c223bb59b855a22d781c87d070e8d7
Tatiana Mosquera <atatianamosquera@gmail.com>
2026-03-28 17:10:04 -0500

    Added new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
     </Routes>
   );
 }
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <>
-    <h1>Holaaa</h1>
+      <h1>Holaaa</h1>
     </>
   );
 }
