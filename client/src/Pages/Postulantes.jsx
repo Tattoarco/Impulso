fb096efd80b1d382283cac3b16b1c2e7fde324ce
Tatiana Mosquera <atatianamosquera@gmail.com>
2026-04-16 20:44:45 -0500

    Errores arreglando

--- client/src/Pages/Postulantes.jsx
+++ client/src/Pages/Postulantes.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/Authcontext";
+
 import SideBar from "../Components/Sidebar";
 import Footer from "../Components/footer";
 
