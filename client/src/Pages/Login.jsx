6a10445df98c94ca179381596026aea8850a1da6
Tatiana Mosquera <atatianamosquera@gmail.com>
2026-04-17 21:27:32 -0500

    Ruta login original

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -18,8 +18,8 @@ export default function Login() {
     setError("");
     setLoading(true);
     try {
-      // const res = await fetch(`${API}/api/auth/login`, {
-      const res = await fetch(`/api/auth/login`, {
+      const res = await fetch(`${API}/api/auth/login`, {
+      // const res = await fetch(`/api/auth/login`, {
       method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
