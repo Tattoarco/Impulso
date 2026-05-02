7272c43642744051d324da086f77514c7e7b9510
Tatiana Mosquera <atatianamosquera@gmail.com>
2026-05-01 22:29:16 -0500

    ayuda

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -112,7 +112,7 @@ export default function Navbar() {
                 <Dropdown.Item id="cerrar-sesion" textValue="Cerrar sesión" variant="danger">
                   <div className="flex items-center gap-2 text-red-500">
                     <div className="w-8 h-8 rounded-full bg-red-100 flex items-center justify-center">
-                      <i className="fi fi-rr-closed text-red-600 text-sm" />
+                      <i className="fi fi-rc-door-closed text-red-600 text-sm" />
                     </div>
                     <Label>Cerrar sesión</Label>
                   </div>
